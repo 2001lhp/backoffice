8af67fedd62d143bbf7677c938c4ef9da84fe52e
lhp <2370033560@qq.cpm>
2023-09-18 18:29:41 +0800

    feat(backoffice): 项目创建

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+// 引入全局注册
+import { globalRegister } from './global'
+
+// 引入mockjs
+import './mock'
+
+const app = createApp(App)
+
+app.use(createPinia()).use(router).use(globalRegister)
+
+app.mount('#app')
